de9f5432560e80749327ee5012ff314086008993
Heus-Sueh <98482469+Heus-Sueh@users.noreply.github.com>
2024-10-04 12:14:16 -0300

    adding more changes and a clipboard

--- files/system/etc/skel/.config/hypr/scripts/clipboard.sh
+++ files/system/etc/skel/.config/hypr/scripts/clipboard.sh
@@ -1,7 +1,59 @@
-#!/bin/env bash
-# Make bash more sane
+#!/usr/bin/env bash
+
 set -euo pipefail
 
-fzf_args="fzf --prompt 'pick > ' --bind 'tab:up' --cycle --read0"
+# Configurations
+term="alacritty"
+theme="$HOME/.local/share/rofi/themes/minimal/style.rasi"
 
-clipman pick --print0 -t CUSTOM -T "$fzf_args"
+# Check which clipboard manager is available
+if command -v cliphist &>/dev/null; then
+	clipboard_manager="cliphist"
+elif command -v clipman &>/dev/null; then
+	clipboard_manager="clipman"
+else
+	echo "No supported clipboard manager found."
+	exit 1
+fi
+
+# Get clipboard content based on the available manager
+case $clipboard_manager in
+cliphist)
+	clipboard_content=$(cliphist list)
+	;;
+clipman)
+	clipboard_content=$(clipman pick -t STDOUT)
+	;;
+*)
+	echo "Unsupported clipboard manager."
+	exit 1
+	;;
+esac
+
+# Use rofi to display clipboard content and select an item
+selected_item=$(echo "$clipboard_content" | rofi \
+	-dmenu \
+	-p "Clipboard History" \
+	-scroll-method 0 \
+	-terminal "$term" \
+	-kb-cancel Escape \
+	-theme "$theme")
+
+# If an item was selected, store it in the clipboard
+if [[ -n "$selected_item" ]]; then
+	case $clipboard_manager in
+	cliphist)
+		# Remove the index and leading whitespace before storing
+		selected_content=$(echo "$selected_item" | awk '{$1=""; print $0}' | sed 's/^ *//')
+		echo -ne "$selected_content" | cliphist store
+		wl-copy "$selected_content" # Copy only the selected text
+		;;
+	clipman)
+		echo -ne "$selected_item" | clipman store
+		;;
+	*)
+		echo "Unsupported clipboard manager."
+		exit 1
+		;;
+	esac
+fi
