cd5bbd92cc32bd1ddb4cf1e79e5c5fdbef5c5787
Heus-Sueh <matheussilvademouraxd@gmail.com>
2024-12-17 12:14:24 -0300

    reverting to use ublue as base-image on all recipes

--- files/scripts/nvidia.sh
+++ files/scripts/nvidia.sh
@@ -13,24 +13,24 @@ sed -i '/#.*env = LIBVA_DRIVER_NAME,nvidia/s/^#\s*//;
 
 echo "Lines successfully uncommented in $file"
 
-# part 2 (set nvidia toolkit policy)
-semodule --verbose --install /usr/share/selinux/packages/nvidia-container.pp
-
-# part 3 (set early loading)
-sed -i 's@omit_drivers@force_drivers@g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
-sed -i 's@ nvidia @ i915 amdgpu nvidia @g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
-
-
-# part 4 (set drm variables)
-echo '
-
-# Nvidia modesetting support. Set to 0 or comment to disable kernel modesetting
-# support. This must be disabled in case of SLI Mosaic.
-
-options nvidia-drm modeset=1 fbdev=1
-
-' > /usr/lib/modprobe.d/nvidia-modeset.conf
-
-cp /usr/lib/modprobe.d/nvidia-modeset.conf /etc/modprobe.d/nvidia-modeset.conf
-
-
+# # part 2 (set nvidia toolkit policy)
+# semodule --verbose --install /usr/share/selinux/packages/nvidia-container.pp
+#
+# # part 3 (set early loading)
+# sed -i 's@omit_drivers@force_drivers@g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
+# sed -i 's@ nvidia @ i915 amdgpu nvidia @g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
+#
+#
+# # part 4 (set drm variables)
+# echo '
+#
+# # Nvidia modesetting support. Set to 0 or comment to disable kernel modesetting
+# # support. This must be disabled in case of SLI Mosaic.
+#
+# options nvidia-drm modeset=1 fbdev=1
+#
+# ' > /usr/lib/modprobe.d/nvidia-modeset.conf
+#
+# cp /usr/lib/modprobe.d/nvidia-modeset.conf /etc/modprobe.d/nvidia-modeset.conf
+#
+#
